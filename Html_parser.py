8ae3413c4ed3c7b3ab640e4c1273630b38d0f180
liujt14 <liujt14@gmail.com>
2018-05-11 22:32:21 +0800

    remove duplicate heads and add contents_sichuan

--- Html_parser.py
+++ Html_parser.py
@@ -5,11 +5,13 @@ import re
 
 page = codecs.open("./明實錄、朝鮮王朝實錄、清實錄資料庫合作建置計畫.html", 'r', 'utf-8')
 soup = BeautifulSoup(page, "lxml")
+page.close()
 contents = []
+heads_all_contents = []
 heads_head = []
 heads_contents = []
 K_number = []
-matches = [['朵顏衛', '朶顏衞', '朶顏衛',  '朵顏衞']]
+matches = [['四川'], ['土官']]
 
 
 def remove_tables_between_divs(table):
@@ -69,63 +71,88 @@ k = 0
 while k < font_numbers:
     div = fonts[k].find_parent('div')
     if div is None:
-        break
-    div_string = div.get_text(strip=True)
-    match_result = True
-
-    for match in matches:
-        if len(match) == 1:
-            match_result_single = [m.start() for m in re.finditer(match[0], div_string)]
-            match_result = bool(len(match_result_single)) and match_result
-        else:
-            sub_match_result = False
-            for sub_match in match:
-                match_result_sub = [m.start() for m in re.finditer(sub_match, div_string)]
-                sub_match_result = bool(len(match_result_sub)) or sub_match_result
-            match_result = sub_match_result and match_result
-    # if len(match1_results) or len(match1_another_results) and len(match2_results):
-    if match_result:
-        font_number_in_div = len(div.findAll('font', {'class': "hit"}))
-        k += font_number_in_div
-        # 向上搜索
-        i = 0
-        for parent in div.parents:
-            if parent is None:
-                print(parent)
-            else:
-                i += 1
-                if i >= 5 and parent.name == 'tr':
-                    # 寻找兄弟节点
-                    previous_tr = parent.find_previous_sibling('tr')
-                    heads = previous_tr.get_text()
-                    # heads optimal
-                    heads_match_results = [m.start() for m in re.finditer('／', heads)]
-                    heads_head.append(heads[0:heads_match_results[2]])
-                    heads_contents.append(heads[(heads_match_results[2]+1):])
-                    div_get_text = div.get_text()
-                    div_text_quanquan = [m.start() for m in re.finditer('○', div_get_text)]
-                    div_text_sanjiao = [m.start() for m in re.finditer('△', div_get_text)]
-                    if len(div_text_quanquan):
-                        div_get_text = div_get_text.replace('○', '')
-                    elif len(div_text_sanjiao):
-                        div_get_text = div_get_text.replace('△', '')
-                    contents.append(div_get_text)
-                    K_number.append(k)
-
-                else:
-                    pass
-            if i > 6:
-                break
-    else:
         k += 1
+        pass
+    else:
+        div_string = div.get_text(strip=True)
+        match_result = True
+
+        for match in matches:
+            if len(match) == 1:
+                match_result_single = [m.start() for m in re.finditer(match[0], div_string)]
+                match_result = bool(len(match_result_single)) and match_result
+            else:
+                sub_match_result = False
+                for sub_match in match:
+                    match_result_sub = [m.start() for m in re.finditer(sub_match, div_string)]
+                    sub_match_result = bool(len(match_result_sub)) or sub_match_result
+                match_result = sub_match_result and match_result
+        if match_result:
+            font_number_in_div = len(div.findAll('font', {'class': "hit"}))
+            k += font_number_in_div
+            # 向上搜索
+            i = 0
+            for parent in div.parents:
+                if parent is None:
+                    print(parent)
+                else:
+                    i += 1
+                    if i >= 5 and parent.name == 'tr':
+                        # 寻找兄弟节点
+                        previous_tr = parent.find_previous_sibling('tr')
+                        heads = previous_tr.get_text()
+                        # heads optimal
+                        heads_all_contents.append(heads)
+                        div_get_text = div.get_text()
+                        div_text_quanquan = [m.start() for m in re.finditer('○', div_get_text)]
+                        div_text_sanjiao = [m.start() for m in re.finditer('△', div_get_text)]
+                        if len(div_text_quanquan):
+                            div_get_text = div_get_text.replace('○', '')
+                        elif len(div_text_sanjiao):
+                            div_get_text = div_get_text.replace('△', '')
+                        contents.append(div_get_text)
+                        K_number.append(k)
+
+                    else:
+                        pass
+                if i > 6:
+                    break
+        else:
+            k += 1
 
 # save to excel:
 
 workbook = xlwt.Workbook(encoding='utf-8')
 worksheet = workbook.add_sheet('1st edition')
+
 sheet_length = len(contents)
-print("found %d rows" % sheet_length)
+print("found %d rows before remove duplicate ones" % sheet_length)
+
+# 去除重复的head,思路,遍历heads_contents 从1开始,如果当前index内容与前一个相同,那么删除当前index
+# 对应的heads,并且将当前index的contents insert到上一个contents 并且删除当前index的contents
+# index +1
+index_for_heads = 1
+while index_for_heads < sheet_length:
+    if index_for_heads >= len(heads_all_contents):
+        break
+    else:
+        if heads_all_contents[index_for_heads] == heads_all_contents[index_for_heads-1]:
+            del heads_all_contents[index_for_heads]
+            contents[index_for_heads-1] = contents[index_for_heads-1] + ';' + contents[index_for_heads]
+            del contents[index_for_heads]
+        index_for_heads += 1
+
+
+sheet_length = len(contents)
+print("found %d rows after remove duplicate ones" % sheet_length)
+
+# for i in range(sheet_length):
+#     if i == 0:
+#         heads_all_contents.append()
 for i in range(sheet_length):
+    heads_match_results = [m.start() for m in re.finditer('／', heads_all_contents[i])]
+    heads_head.append(heads_all_contents[i][0:heads_match_results[2]])
+    heads_contents.append(heads_all_contents[i][(heads_match_results[2] + 1):])
     worksheet.write(i, 0, i+1)
     worksheet.write(i, 1, heads_head[i])
     worksheet.write(i, 2, heads_contents[i])
@@ -142,4 +169,4 @@ for match in matches:
             file_name += sub_match
 
 workbook.save('./Excel_%s.xls' % file_name)
-page.close()
+
